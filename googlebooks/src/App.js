09866b1ff9e3a3fc76af5e4a9cafc1d2b68d19b5
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-06-25 12:18:15 -0600

    initial commit

---
+++ googlebooks/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4"></hr>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a className="btn btn-primary btn-lg" href="https://github.com/cwp132/React-Book-Search" role="button">Learn more</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
